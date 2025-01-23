78e06c5faa631e73ed080827247595e19f3ae815
Bminor87 <jessehellman@gmail.com>
2025-01-23 17:36:58 +0200

    task 4 done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,100 +1,93 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import {
   TextInput,
   Text,
   TouchableOpacity,
   useColorScheme,
   FlatList,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
 } from "react-native";
 import styles from "../gui/styles";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
-  const [input1, setInput1]: [string, any] = useState("");
-  const [input2, setInput2]: [string, any] = useState("");
-  const result = useRef(0);
-  const [message, setMessage]: [any, any] = useState(0);
-  const [history, setHistory]: [string[], any] = useState([]);
+  const [input, setInput]: [string, any] = useState("");
+  const [shoppinglist, setShoppinglist]: [string[], any] = useState([]);
 
   const colorScheme = useColorScheme();
 
-  const operation = (operation: "+" | "-") => {
-    result.current =
-      operation === "+"
-        ? parseInt(input1) + parseInt(input2)
-        : parseInt(input1) - parseInt(input2);
-
-    if (isNaN(result.current)) {
-      setMessage("Käytä vain numeroita!");
+  const addShoppingItem = () => {
+    if (input === "") {
+      Alert.alert("Virhe", "Tuote ei voi olla tyhjä");
       return;
     }
+    setShoppinglist([...shoppinglist, input]);
+    setInput("");
+  };
 
-    const newMessage = `${input1} ${operation} ${input2} = ${result.current}`;
-
-    setMessage(newMessage);
-    setHistory([newMessage, ...history]);
+  const clearShoppingList = () => {
+    setShoppinglist([]);
   };
 
   return (
-    <ThemedView style={styles.appContainer}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Laskin</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">{message}</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <TextInput
-          style={[
-            styles.input,
-            {
-              color: colorScheme === "dark" ? "white" : "black",
-              borderColor: colorScheme === "dark" ? "white" : "gray",
-            },
-          ]}
-          onChangeText={setInput1}
-          value={input1.toString()}
-          keyboardType="numeric"
-          placeholder="Syötä numero"
-        />
-        <TextInput
-          style={[
-            styles.input,
-            {
-              color: colorScheme === "dark" ? "white" : "black",
-              borderColor: colorScheme === "dark" ? "white" : "gray",
-            },
-          ]}
-          onChangeText={setInput2}
-          value={input2.toString()}
-          keyboardType="numeric"
-          placeholder="Syötä toinen numero"
-        />
-      </ThemedView>
-      <ThemedView style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.customButton}
-          onPress={() => operation("+")}
-        >
-          <Text style={styles.buttonText}>+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.customButton}
-          onPress={() => operation("-")}
-        >
-          <Text style={styles.buttonText}>-</Text>
-        </TouchableOpacity>
-      </ThemedView>
-
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Historia</ThemedText>
-        <FlatList
-          data={history}
-          renderItem={({ item }) => <ThemedText>{item}</ThemedText>}
-          keyExtractor={(item, index) => index.toString()}
-        />
-      </ThemedView>
-    </ThemedView>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"} // Apparently ios and android handle this differently
+    >
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <ThemedView style={styles.appContainer}>
+          <ThemedView style={styles.titleContainer}>
+            <ThemedText type="title">Ostoslista</ThemedText>
+          </ThemedView>
+          <ThemedView style={styles.stepContainer}>
+            <ThemedText type="subtitle">&nbsp;</ThemedText>
+            <FlatList
+              data={shoppinglist}
+              renderItem={({ item }) => <ThemedText>{item}</ThemedText>}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </ThemedView>
+          <ThemedView style={styles.stepContainer}>
+            <ThemedText type="subtitle">&nbsp;</ThemedText>
+          </ThemedView>
+          <ThemedView style={styles.stepContainer}>
+            <TextInput
+              style={[
+                styles.input,
+                {
+                  color: colorScheme === "dark" ? "white" : "black",
+                  borderColor: colorScheme === "dark" ? "white" : "gray",
+                },
+              ]}
+              onChangeText={setInput}
+              onSubmitEditing={addShoppingItem}
+              value={input.toString()}
+              placeholder="ostoskoriin lisättävä tuote"
+            />
+          </ThemedView>
+          <ThemedView style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.customButton}
+              onPress={addShoppingItem}
+            >
+              <Text style={styles.buttonText}>Lisää</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.customButton}
+              onPress={clearShoppingList}
+            >
+              <Text style={styles.buttonText}>Tyhjennä</Text>
+            </TouchableOpacity>
+          </ThemedView>
+        </ThemedView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
