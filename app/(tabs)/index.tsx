5063308f0edc03ae93fde001dc1798e3c039304c
Bminor87 <jessehellman@gmail.com>
2025-01-23 17:19:25 +0200

    task 3 done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,48 @@
-import { useState, useEffect } from "react";
+import { useState, useRef } from "react";
 import {
-  Image,
-  StyleSheet,
   TextInput,
   Text,
   TouchableOpacity,
   useColorScheme,
+  FlatList,
 } from "react-native";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
+import styles from "../gui/styles";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
-  const [input1, setInput1] = useState("");
-  const [input2, setInput2] = useState("");
-  const [result, setResult]: any = useState(0);
+  const [input1, setInput1]: [string, any] = useState("");
+  const [input2, setInput2]: [string, any] = useState("");
+  const result = useRef(0);
+  const [message, setMessage]: [any, any] = useState(0);
+  const [history, setHistory]: [string[], any] = useState([]);
 
   const colorScheme = useColorScheme();
 
-  const add = () => setResult(parseInt(input1) + parseInt(input2));
-  const subtract = () => setResult(parseInt(input1) - parseInt(input2));
+  const operation = (operation: "+" | "-") => {
+    result.current =
+      operation === "+"
+        ? parseInt(input1) + parseInt(input2)
+        : parseInt(input1) - parseInt(input2);
+
+    if (isNaN(result.current)) {
+      setMessage("Käytä vain numeroita!");
+      return;
+    }
+
+    const newMessage = `${input1} ${operation} ${input2} = ${result.current}`;
+
+    setMessage(newMessage);
+    setHistory([newMessage, ...history]);
+  };
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
+    <ThemedView style={styles.appContainer}>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Laskin</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">
-          Tulos: {isNaN(result) ? "Käytä vain numeroita!" : result}
-        </ThemedText>
+        <ThemedText type="subtitle">{message}</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <TextInput
@@ -68,53 +73,28 @@ export default function HomeScreen() {
         />
       </ThemedView>
       <ThemedView style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.customButton} onPress={add}>
+        <TouchableOpacity
+          style={styles.customButton}
+          onPress={() => operation("+")}
+        >
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.customButton} onPress={subtract}>
+        <TouchableOpacity
+          style={styles.customButton}
+          onPress={() => operation("-")}
+        >
           <Text style={styles.buttonText}>-</Text>
         </TouchableOpacity>
       </ThemedView>
-    </ParallaxScrollView>
+
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Historia</ThemedText>
+        <FlatList
+          data={history}
+          renderItem={({ item }) => <ThemedText>{item}</ThemedText>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </ThemedView>
+    </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    gap: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-  input: {
-    height: 40,
-    paddingLeft: 8,
-    borderWidth: 1,
-    marginBottom: 8,
-  },
-  customButton: {
-    backgroundColor: "#007BFF",
-    padding: 10,
-    borderRadius: 5,
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "bold",
-  },
-});
